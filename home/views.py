d23d631d2ba6670fa6608b891f84591afea4b6f8
Damien Wensel <damienw@hr.upenn.edu>
2024-10-15 12:48:15 -0400

    Completed Chapter 4

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-
-@login_required(login_url='/admin') # If the user is not logged in, redirect to login_url
-def authorized(request):
-    return render(request, 'home/authorized.html',{})
+class AuthoerizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name ="notes" # Default is objects but we called it 'notes'
+    # This class view works without this explicit template name as we followed the default naming convention, but this is how we would specify if we didn't
+    template_name = "notes/notes_list.html" 
 
+# These class base views handle exceptions so we don't need to handle that complexity
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+# This is an example of our old function detail view and the added complexity versus the class based view
+"""
 def detail(request, pk):
     try:
         note = Notes.objects.get(pk=pk)
     except Notes.DoesNotExist:
         raise Http404("Note doesn't exist")
     return render(request, 'notes/notes_detail.html', {'note': note})
+"""
